904881abbdf94f4b2639b191a73969a238236884
Tadeus Vito <gavravitto@gmail.com>
2025-05-26 22:24:38 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,15 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import LapanganRoutes from "./routes/LapanganRoutes.js";
 import BookingRoutes from "./routes/BookingRoutes.js";
 import UserRoutes from "./routes/UserRoutes.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
-app.use(cookieParser());
+app.use(cors());
 app.use(express.json());
-
 app.use(LapanganRoutes);
 app.use(BookingRoutes);
 app.use(UserRoutes);
+
+app.listen(5000, () => console.log('Server telah berjalan di port 5000'));
