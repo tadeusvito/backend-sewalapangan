bf545156dc848c804fad2a8d2ccf17011a5c3bb2
Tadeus Vito <gavravitto@gmail.com>
2025-05-26 22:42:07 +0700

    fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
 });
